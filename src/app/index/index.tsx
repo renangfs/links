713ec2077e56043b58f1db1c3437bfee5c5537be
Renan Germano <renangfs77@gmail.com>
2025-03-05 14:09:09 -0300

    teste alteração

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,8 +5,8 @@ import { styles } from "./styles"
 export default function Index(){
     return(
         <View style={styles.container}>
-            <Text style={styles.Title}>Hello</Text>
-            <Text style={styles.Title}>React Native!</Text>
+            <Text style={styles.Title}>Hello renan</Text>
+            <Text style={styles.Title}>React renan Native!</Text>
         </View>
         )
 
