f9f99461fbeb924d1604cfbadb5fe1c136ba6eda
Renan Germano <renangfs77@gmail.com>
2025-03-05 14:01:32 -0300

    primeiro commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View, } from "react-native"
+
+import { styles } from "./styles"
+
+export default function Index(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.Title}>Hello</Text>
+            <Text style={styles.Title}>React Native!</Text>
+        </View>
+        )
+
+}
---
+++ src/app/index/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native"
+
+import { colors } from "../../styles/colors"
+
+export const styles =StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        //flexDirection: "row",
+    },
+    Title:{
+        color: colors.green[900],
+        fontSize: 22,
+    },
+})
